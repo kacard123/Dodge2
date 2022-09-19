be75b3fdcbb3e1388bf30471951c572db86b4199
kacard123 <99639271+kacard123@users.noreply.github.com>
2022-09-19 12:35:55 +0900

    20220919-01

---
+++ Dodge/Assets/Bullet.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody bulletRigidbody; // 이동에 사용할 리지드바디 컴포넌트
+    public float speed = 8f; // 탄알 이동 속력
+
+    void Start()
+    {
+        
+    }
+
+    
+}
