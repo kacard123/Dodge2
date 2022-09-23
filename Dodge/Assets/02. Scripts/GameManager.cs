0b87b7821d9867d36062ac3b50c0035e115ddc9f
kacard123 <99639271+kacard123@users.noreply.github.com>
2022-09-23 17:30:58 +0900

    20220923-last

--- Dodge/Assets/02. Scripts/GameManager.cs
+++ Dodge/Assets/02. Scripts/GameManager.cs
@@ -8,8 +8,8 @@ using TMPro; // TextMeshPro 관련 라이브러리
 public class GameManager : MonoBehaviour
 {
     public GameObject gameoverText; // 게임오버 시 활성화할 텍스트 게임 오브젝트
-    public TextMeshPro timeText; // 생존 시간을 표시할 텍스트 컴포넌트
-    public TextMeshPro recordText; // 최고 기록을 표시할 텍스트 컴포넌트
+    public TextMeshProUGUI timeText; // 생존 시간을 표시할 텍스트 컴포넌트
+    public TextMeshProUGUI recordText; // 최고 기록을 표시할 텍스트 컴포넌트
 
     private float surviveTime; // 생존 시간
     private bool isGameOver; // 게임오버 상태
@@ -26,7 +26,7 @@ public class GameManager : MonoBehaviour
         // 게임오버가 아닌 동안
         if(!isGameOver)
         {
-            // 생존 시간 갱신
+            // 생존 시간 갱신 -> 생존 시간은 누적될 수 없다
             surviveTime += Time.deltaTime;
             // 갱신한 생존 시간을 timeText 텍스트 컴포넌트를 이용해 표시
             timeText.text = "Time: " + (int)surviveTime;
@@ -50,5 +50,20 @@ public class GameManager : MonoBehaviour
         isGameOver = true;
         // 게임오버 텍스트 게임 오브젝트를 활성화
         gameoverText.SetActive(true);
+
+        // BestTime 키로 저장된 이전까지의 최고 기록 가져오기
+        float bestTime = PlayerPrefs.GetFloat("BestTime");
+
+        // 이전까지의 최고 기록보다 현재 생존 시간이 더 크다면
+        if(surviveTime > bestTime)
+        {
+            // 최고 기록 값을 현재 생존 시간 값으로 변경
+            bestTime = surviveTime;
+            // 변경된 최고 기록을 BestTime 키로 저장
+            PlayerPrefs.SetFloat("BestTime", bestTime);
+        }
+
+        // 최고 기록을 recordText 텍스트 컴포넌트를 이용해 표시
+        recordText.text = "BestTime" + (int)bestTime;
     }
 }
